d9166d37668a85f742d1f5c9972bdef1aa6e9e77
ellen <ellen.vitoria.ogs@gmail.com>
2021-11-17 17:39:31 -0300

    Adicionando estrutura do Controller HomeController.java

--- src/main/java/com/example/springMVC/controllers/HomeController.java
+++ src/main/java/com/example/springMVC/controllers/HomeController.java
@@ -1,5 +1,13 @@
 package com.example.springMVC.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
 
+    @GetMapping("/index")
+    public String HTML(){
+        return "index";
+    }
 }
