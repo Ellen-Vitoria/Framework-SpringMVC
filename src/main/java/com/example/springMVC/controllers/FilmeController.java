f679d4b28781cfb424b75a1dd28a82b8781f3470
ellen <ellen.vitoria.ogs@gmail.com>
2021-11-17 15:56:43 -0300

    Adicionando estrutura do Controller FilmeController.java

--- src/main/java/com/example/springMVC/controllers/FilmeController.java
+++ src/main/java/com/example/springMVC/controllers/FilmeController.java
@@ -1,4 +1,22 @@
 package com.example.springMVC.controllers;
 
+import com.example.springMVC.models.Filme;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class FilmeController {
+
+    private List<Filme> filmes = List.of(
+            new Filme("Capitã Marvel", "O filme conta a história...", "Ficção Científica", "2019", "2h 4min"),
+            new Filme("Doutor Estranho", "O filme conta a história...", "Ficção Científica", "2016", "1h 55min"),
+            new Filme("Guardiões da Galáxia Vol. 2", "O filme conta a história...", "Ficção Científica", "2017", "2h 17min")
+    );
+
+    @GetMapping("/filmes")
+    public List<Filme> getFilmes(){
+        return filmes;
+    }
 }
